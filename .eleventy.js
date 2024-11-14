a6f515e50f1ad5ae480b0243d8b31b3cf7c6e284
ankudovychm <ankudovych.m@northeastern.edu>
2024-11-14 12:58:09 -0500

    fixed the file duplication issue

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,16 @@
 module.exports = function(eleventyConfig) {
-    // Pass through "styles" and "images" folders to the output folder
-    eleventyConfig.addPassthroughCopy("src/styles");
-    eleventyConfig.addPassthroughCopy("src/images");
-    eleventyConfig.addPassthroughCopy("src/JsScripts");
-    eleventyConfig.addPassthroughCopy("src/data");
-
-    
-    
   
-    return {
-        dir: {
-          input: "src",
-          output: "_site",
-          includes: "_includes"
-        }
-      };
-    };
+  // Pass through these directors and any sibdirectories
+  eleventyConfig.addPassthroughCopy("src/data/**/*");
+  eleventyConfig.addPassthroughCopy("src/JsScripts/**/*");
+  eleventyConfig.addPassthroughCopy("src/images/**/*");
+  eleventyConfig.addPassthroughCopy("src/styles/**/*");
+
+  return {
+      dir: {
+          input: "src",   // Input folder (all your source files)
+          output: "_site",  // Output folder
+          includes: "_includes"  // Folder for templates and includes
+      }
+  };
+};
--- _site/JsScripts/gtime/d3 8.js
+++
@@ -1,8 +0,0 @@
-export { map, max, min, extent } from "d3-array";
-export { axisBottom, axisLeft, axisRight } from "d3-axis";
-export { color } from "d3-color";
-export { drag } from "d3-drag";
-export { scaleBand, scaleLinear, scaleOrdinal, scaleTime } from "d3-scale";
-export { select, selectAll } from "d3-selection";
-export { isoParse, timeFormat } from "d3-time-format";
-export { active } from "d3-transition";
