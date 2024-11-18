09c1fd6031b91dbec502289847ec4e6e36ad267b
ankudovychm <ankudovych.m@northeastern.edu>
2024-11-18 13:41:09 -0500

    tools pages uses pagination

---
+++ src/_data/tools.js
@@ -0,0 +1,16 @@
+module.exports = {
+    tools: [
+        {
+            tool: "coref",
+            title: "Person Co-Reference Network Graphs",
+        },
+        {
+            tool: "geo",
+            title: "Geo-Reference Maps",
+        },
+        {
+            tool: "timeline",
+            title: "Subject Reference Timeline",
+        },
+    ],
+};
