2efe89f2496800a52e47dac455227699c9e6695c
ankudovychm <ankudovych.m@northeastern.edu>
2024-11-19 16:23:14 -0500

    testing yaml

--- src/JsScripts/timeline/d3.js
+++ src/JsScripts/timeline/d3.js
@@ -3,6 +3,6 @@ export { axisBottom, axisLeft, axisRight } from "d3-axis";
 export { color } from "d3-color";
 export { drag } from "d3-drag";
 export { scaleBand, scaleLinear, scaleOrdinal, scaleTime } from "d3-scale";
-export { select } from "d3-selection";
+export { select, selectAll } from "d3-selection";
 export { isoParse, timeFormat } from "d3-time-format";
 export { active } from "d3-transition";
